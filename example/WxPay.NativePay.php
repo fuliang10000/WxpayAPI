c9df431091d44cbdc3c7cd03e7f37f424bac2fb0
molin <wudaxing0829@126.com>
2018-02-06 11:36:41 +0800

    提交测试 Signed-off-by: molin <wudaxing0829@126.com>

--- example/WxPay.NativePay.php
+++ example/WxPay.NativePay.php
@@ -47,10 +47,11 @@ class NativePay
 	 */
 	public function GetPayUrl($input)
 	{
-		var_dump($input->GetTrade_type());die;
 		if($input->GetTrade_type() == "NATIVE")
 		{
 			$result = WxPayApi::unifiedOrder($input);
+			var_dump($result);die;
+
 			return $result;
 		}
 	}
